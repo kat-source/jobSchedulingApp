ffb6baf510c4830e326fd0c1fb4bf9b798d1a69d
kat-source <kathleenkarlotte.millan@biotechfarms.net>
2022-06-06 15:01:37 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Jobs />
+</template>
+
+<script>
+import sidebar from "~/layouts/sidebar";
+export default {
+  name: 'IndexPage',
+  components: {
+    sidebar,
+    }
+}
+</script>
